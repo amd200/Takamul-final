3e381adabe02cebbc76cd09a1e6f7c62a14d96a2
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-05-04 23:29:35 +0300

    done

--- src/lib/initAuth.ts
+++ src/lib/initAuth.ts
@@ -1,16 +1,21 @@
-// initAuth.ts
 import { useAuthStore } from "@/store/authStore";
 import { jwtDecode } from "jwt-decode";
 import { AppJwtPayload } from "@/types";
 import { LoginResponse } from "@/features/auth/types/auth.types";
 import { apiClient } from "@/api/client";
 
+const authChannel = new BroadcastChannel("auth");
+authChannel.onmessage = (event) => {
+  if (event.data?.type === "LOGOUT") {
+    useAuthStore.getState().clearAuth();
+    window.location.href = "/login";
+  }
+};
+
 export const initAuth = async (): Promise<void> => {
   try {
     const { data } = await apiClient.post<LoginResponse>("/Auth/refresh-token");
     const decoded = jwtDecode<AppJwtPayload>(data.accessToken);
-    console.log(data?.accessToken);
-    console.log(decoded);
     useAuthStore.getState().setAuth(data.accessToken, new Date(data.accessTokenExpiration).getTime(), decoded.Permission, decoded?.UserId, decoded?.email, decoded?.username);
   } catch {
     useAuthStore.getState().clearAuth();
